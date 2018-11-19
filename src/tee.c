c1b8dfb1b60c954f40fcfa9538ad3ce161e8ea02
Brandon Foltz <bsfoltzguitar@gmail.com>
2018-11-18 21:49:48 -0500

    Work in progress. Getting segfaults currently.

--- src/tee.c
+++ src/tee.c
@@ -21,6 +21,13 @@
 #include <signal.h>
 #include <getopt.h>
 
+//additions for Teep functionality
+#include <pthread.h>
+
+//TODO FIXME temporary for debugging
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "system.h"
 #include "argmatch.h"
 #include "die.h"
@@ -35,8 +42,25 @@
 #define AUTHORS \
   proper_name ("Mike Parker"), \
   proper_name ("Richard M. Stallman"), \
-  proper_name ("David MacKenzie")
+  proper_name ("David MacKenzie"), \
+  proper_name ("Brandon Foltz")
 
+//this structure is shared by each worker BUT each worker has it's own descriptor
+typedef struct parallel_tee_params_s {
+	char * buffer;
+	ssize_t * bytes_read;
+	FILE * descriptor;
+	char ** files;
+	
+	pthread_mutex_t * mutex;
+	pthread_cond_t * writable;
+	int * numWritersWaiting;
+	int threadIndex;
+	int numThreads;
+	bool * canContinue;
+} parallel_tee_params_t; 
+
+static void * parallel_tee(parallel_tee_params_t * params);
 static bool tee_files (int nfiles, char **files);
 
 /* If true, append to output files rather than truncating them. */
@@ -114,6 +138,21 @@ writing to non pipe outputs.\n\
   exit (status);
 }
 
+//TODO FIXME delete me! for debugging only!
+long debugPrint(char * string); //prototype just to make the warning go away
+pthread_mutex_t printMutex;
+long debugPrint(char * string) {
+    static long timestamp = 0;
+    pthread_mutex_lock(&printMutex);
+    timestamp++;
+    FILE *fp = fopen("/var/tmp/teep_debug.txt", "a"); 
+    fprintf(fp, "%ld: %s", timestamp, string);
+    fflush(fp);
+    fclose(fp);
+    pthread_mutex_unlock(&printMutex);
+    return timestamp;    
+}
+
 int
 main (int argc, char **argv)
 {
@@ -169,11 +208,74 @@ main (int argc, char **argv)
   /* Do *not* warn if tee is given no file arguments.
      POSIX requires that it work when given no arguments.  */
 
+  pthread_mutex_init(&printMutex, NULL); //TODO FIXME delete me!
+
   ok = tee_files (argc - optind, &argv[optind]);
   if (close (STDIN_FILENO) != 0)
     die (EXIT_FAILURE, errno, "%s", _("standard input"));
 
   return ok ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+static void *
+parallel_tee(parallel_tee_params_t * params)
+{
+	char stringBuffer[128] = {0};
+	sprintf(stringBuffer, "Thread for file %s started.\n", (*params).files[(*params).threadIndex]);
+	debugPrint(stringBuffer);
+	static bool ok = true;
+	bool canContinue = true;
+	bool refilledBuffer = false;
+	while (canContinue) {
+		//TODO Tee writing goes here
+		if ((*params).descriptor
+		&& fwrite ((*params).buffer, *(*params).bytes_read, 1, (*params).descriptor) != 1)
+		{
+			int w_errno = errno;
+			bool fail = errno != EPIPE || (output_error == output_error_exit
+							              || output_error == output_error_warn);
+			if ((*params).descriptor == stdout)
+			  clearerr (stdout); // Avoid redundant close_stdout diagnostic.  
+			if (fail)
+			{
+				error (output_error == output_error_exit
+				   || output_error == output_error_exit_nopipe,
+				   w_errno, "%s", quotef ((*params).files[(*params).threadIndex]));
+			}
+			(*params).descriptor = NULL;
+			if (fail) {
+			  ok = false;
+			  //n_outputs--; //FIXME This indicates we should quit? Go in critical section?
+			  //puts("One thread failed!"); //FIXME only for debugging
+			}
+		}
+	
+		pthread_mutex_lock((*params).mutex);
+		*(*params).numWritersWaiting += 1;
+		if (*(*params).numWritersWaiting == (*params).numThreads) {
+			
+			//refill the buffer, and trigger all threads quitting if necessary
+			*(*params).bytes_read = read (0, (*params).buffer, sizeof (*params).buffer);
+		    if (*(*params).bytes_read < 0 && errno == EINTR)
+			  *(*params).canContinue = true;
+		    if (*(*params).bytes_read <= 0) {
+			  *(*params).canContinue = false;
+			}
+			canContinue = *(*params).canContinue; //signalling other threads to quit (or not)
+			
+			*(*params).numWritersWaiting = 0;
+			refilledBuffer = true; 	
+		} else {
+			pthread_cond_wait((*params).writable, (*params).mutex);
+		}
+		pthread_mutex_unlock((*params).mutex);
+		if (refilledBuffer) { //avoid spurious wakeup by releasing lock before broadcast
+			pthread_cond_broadcast((*params).writable);
+			refilledBuffer = false;
+		}
+	}    
+
+	return &ok;
 }
 
 /* Copy the standard input into each of the NFILES files in FILES
@@ -183,6 +285,7 @@ main (int argc, char **argv)
 static bool
 tee_files (int nfiles, char **files)
 {
+  puts("Tee files"); //TODO FIXME debugging only
   size_t n_outputs = 0;
   FILE **descriptors;
   char buffer[BUFSIZ];
@@ -206,8 +309,11 @@ tee_files (int nfiles, char **files)
   descriptors[0] = stdout;
   files[0] = bad_cast (_("standard output"));
   setvbuf (stdout, NULL, _IONBF, 0);
-  n_outputs++;
+  n_outputs++; //standard output is always in the set of outputs
 
+  char stringBuffer[128] = {0};
+  sprintf(stringBuffer, "315: n_outputs: %d, nfiles: %d\n", n_outputs, nfiles);
+  debugPrint(stringBuffer);
   for (i = 1; i <= nfiles; i++)
     {
       /* Do not treat "-" specially - as mandated by POSIX.  */
@@ -225,38 +331,63 @@ tee_files (int nfiles, char **files)
           n_outputs++;
         }
     }
-
-  while (n_outputs)
-    {
-      bytes_read = read (0, buffer, sizeof buffer);
-      if (bytes_read < 0 && errno == EINTR)
-        continue;
-      if (bytes_read <= 0)
-        break;
-
-      /* Write to all NFILES + 1 descriptors.
-         Standard output is the first one.  */
-      for (i = 0; i <= nfiles; i++)
-        if (descriptors[i]
-            && fwrite (buffer, bytes_read, 1, descriptors[i]) != 1)
-          {
-            int w_errno = errno;
-            bool fail = errno != EPIPE || (output_error == output_error_exit
-                                          || output_error == output_error_warn);
-            if (descriptors[i] == stdout)
-              clearerr (stdout); /* Avoid redundant close_stdout diagnostic.  */
-            if (fail)
-              {
-                error (output_error == output_error_exit
-                       || output_error == output_error_exit_nopipe,
-                       w_errno, "%s", quotef (files[i]));
-              }
-            descriptors[i] = NULL;
-            if (fail)
-              ok = false;
-            n_outputs--;
-          }
-    }
+    
+  sprintf(stringBuffer, "336: n_outputs: %ld, nfiles: %ld\n", n_outputs, nfiles);
+  debugPrint(stringBuffer);  
+    
+  //descriptors are open and ready to go, read in the first data buffer
+  bytes_read = read(0, buffer, sizeof buffer);
+  bool canContinue = true;
+  if (bytes_read < 0 && errno == EINTR) {
+    canContinue = true;
+  }
+  if (bytes_read <= 0) {
+    canContinue = false; 
+  }
+   
+  sprintf(stringBuffer, "348: Can continue: %d, bytes_read: %d, errno: %d\n", (int)canContinue, bytes_read, errno);
+  debugPrint(stringBuffer);   
+   
+  if (canContinue) { 
+	  //setup and spawn worker threads 
+	  pthread_t workers[nfiles];
+	  parallel_tee_params_t tee_params[nfiles]; 
+	  
+	  //these variables are shared among the threads
+	  pthread_mutex_t mutex;
+	  pthread_cond_t writable;
+	  pthread_mutex_init(&mutex, NULL);
+	  pthread_cond_init(&writable, NULL);
+	  int numWritersWaiting = 0;
+	  for (i = 0; i <= nfiles; i++) { 
+	  	if (descriptors[i]) { //don't start threads for bad descriptors!
+		  	tee_params[i].buffer = buffer;
+		  	tee_params[i].bytes_read = &bytes_read;
+		  	tee_params[i].descriptor = descriptors[i];
+		  	tee_params[i].files = files;
+		  	tee_params[i].mutex = &mutex;
+		  	tee_params[i].writable = &writable;
+		  	tee_params[i].numWritersWaiting = &numWritersWaiting;
+		  	tee_params[i].threadIndex = i;
+		  	tee_params[i].numThreads = nfiles;
+		  	tee_params[i].canContinue = &canContinue; //naughty variable re-use
+		  	char stringBuffer[128] = {0};
+		  	sprintf(stringBuffer, "Starting a thread for descriptor %d\n", i);
+		  	debugPrint(stringBuffer);
+		  	pthread_create(&workers[i], NULL, (void *)parallel_tee, &tee_params[i]);
+	    }
+	  }
+  
+	  //harvest finished threads
+	  //bool retval[nfiles];
+	  for (i = 0; i <= nfiles; i++) {
+	  	char stringBuffer[128] = {0};
+	  	sprintf(stringBuffer, "Waiting thread %d to die...\n", i);
+	  	debugPrint(stringBuffer);
+	  	
+	  	pthread_join(workers[i], (void *)NULL); //&retval[i]);
+	  }
+  }
 
   if (bytes_read == -1)
     {
